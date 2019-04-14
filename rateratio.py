1137a681c68547f7ff4a94c1a769123c0a5a8700
zcx <zcx@umich.edu>
2019-04-13 22:32:09 -0400

    first commit

---
+++ rateratio.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python
+docstring='''
+rateratio.test\tAn Exact Rate Ratio Test Assuming Poisson Counts
+
+Description
+    Performs the uniformy most powerful unbiased test on the ratio of rates
+    of two Poisson counts with given time (e.g., perons-years) at risk for
+    each count.
+
+Usage
+    from rateratio import test as rateratio_test
+    rateratio_test(x, n, RR = 1, 
+        alternative = "two.sided",
+        conf_level = 0.95)
+
+Arguments
+    x               a vector of length 2 with counts for the two rates
+    n               a vector of length 2 with time at risk in each rate
+    RR              the null rate ratio (two.sided) or the rate ratio on
+                    boundary between null and alternative
+    alternative     a character string specifying the alternative hypothesis,
+                    must be one of "two.sided" (default), "greater" or
+                    "less".  You can specify just the initial letter.
+    conf_level      confidence level of the returned confidence interval.
+                    Must be a single number between 0 and 1.
+
+Details
+    The rateratio.test tests whether the ratio of the first rate (estimated
+    by x[0]/n[0] over the second rate (estimated by x[1]/n[1]) is either
+    equal to, less, or greater than RR. The two-sided p-value is defined as
+    either 1 or twice the minimum of the one-sided p-values.
+    See Lehmann (1986, p. 152). For full discussion of the p-value and
+    confidence interval consistency of inferences, see Fay (2010).
+
+Value
+    An object of class "htest" containing the following components:
+    p_value         the p-value of the test
+    estimate        a vector with the rate ratio and the two individual rates
+    null_value      the null rate ratio (two.sided) or the rate ratio on
+                    boundary between null and alternative
+    conf_int        confidence interval
+    alternative     type of alternative hypothesis
+    methods         description of method
+    data_name       description of data
+
+References
+    Fay, M. P. (2010). Two-sided exact tests and matching confidence
+        intervals for discrete data. R Journal, 2(1), 53-58.
+    Lehmann, E.L. (1986). Testing Statistical Hypotheses (second edition).
+        Wadsworth and Brooks/Cole, Pacific Grove, California. }
+
+Examples
+    from rateratio import test as rateratio_test
+    print(rateratio_test((2,9), (17877,16660)))
+'''
+
+from scipy.stats import binom
+pbinom = binom.cdf
+from scipy.stats import beta
+qbeta = beta.ppf
+from numpy import Inf
+
+class htest:
+    def __init__(self, x, n, RR, alternative):
+        self.p_value=0
+        self.estimate=[] # "Rate Ratio","Rate 1","Rate 2"
+        self.null_value=RR # RR "rate ratio"
+        self.conf_int=[]
+        self.alternative=alternative
+        self.method="Exact Rate Ratio Test, assuming Poisson counts"
+        self.data_name="data:  c(%d, %d) with time of c(%d, %d), null rate ratio "%(x[0],x[1],n[0],n[1])+str(RR)
+        return
+
+    def __repr__(self):
+        hypothesis = "not equal to "+str(self.null_value)
+        if   self.alternative == "greater":
+            hypothesis = "greater than "+str(self.null_value)
+        elif self.alternative == "less":
+            hypothesis = "less than "+str(self.null_value)
+        return '''
+\t%s
+
+%s
+p-value = %s
+alternative hypothesis: true rate ratio is %s
+%s percent confidence interval:
+ %s %s
+sample estimates:
+  Rate Ratio       Rate 1       Rate 2
+%s\t%s\t%s
+'''%(self.method,
+    self.data_name,
+    str(self.p_value),
+    hypothesis,
+    str(100*self.conf_int[-1]),
+    self.conf_int[0],self.conf_int[1],
+    self.estimate[0],self.estimate[1],self.estimate[2],
+    )
+
+    def __str__(self):
+        return self.__repr__()
+
+# Modify p.L and p.U from binom.test function
+def p_L(x,n,alpha):
+    if x == 0:
+       return 0
+    return qbeta(alpha, x, n - x + 1)
+
+def p_U(x,n,alpha):
+    if x == n:
+       return 1
+    return qbeta(1 - alpha, x + 1, n - x)
+
+def test(x, n, RR = 1, alternative = "two.sided", conf_level = 0.95):
+    '''
+    x           - a vector of length 2 with counts for the two rates
+    n           - a vector of length 2 with time at risk in each rate
+    RR          - the null rate ratio (two.sided) or the rate ratio on
+                  boundary between null and alternative
+    alternative - a string specifying the alternative hypothesis, must be
+                  one of "two.sided" (default), "greater" or "less".
+                  You can specify just the initial letter.
+    conf_level  - confidence level of the returned confidence interval.
+                  Must be a single number between 0 and 1.
+    '''
+    # modify checks from prop.test
+    k = len(x)
+    if k != 2:
+        raise("x must have a length 2")
+    if k != len(n):
+        raise("'x' and 'n' must have the same length")
+    if min(n) <= 0:
+        raise("elements of 'n' must be positive")
+    if min(x) < 0:
+        raise("elements of 'x' must be nonnegative")
+    if RR <= 0:
+        raise("RR must be greater than 0")
+    
+    #alternative <- match.arg(alternative)
+    if   alternative == "t" or alternative == "two_sided":
+        alternative = "two.sided"
+    elif alternative == "g":
+        alternative = "greater"
+    elif alternative == "l":
+        alternative = "less"
+    if not alternative in ("two.sided","less","greater"):
+        raise('''Error in match.arg(alternative) : 
+  'arg' should be one of "two.sided", "less", "greater"''')
+
+    if conf_level <= 0 or conf_level >= 1:
+        raise("'conf.level' must be a single number between 0 and 1")
+
+    RVAL=htest(x, n, RR, alternative)
+
+    Y = x[0]
+    N = n[0]
+    X = x[1]
+    M = n[1]
+    RVAL.estimate = [ (1.*Y/N)/(1.*X/M), 1.*Y/N, 1.*X/M ]
+    pRR =  (1.*N*RR)/(N*RR + M)
+    pval_less =  pbinom(Y, X+Y, pRR)
+    pval_greater = 1 - pbinom(Y-1, Y+X, pRR)
+
+    if   alternative == "less":
+        RVAL.p_value  = pval_less
+        RVAL.conf_int = [
+            0, (p_U(Y,X+Y,1-conf_level)*M)/(N*(1-p_U(Y,X+Y,1-conf_level) ))]
+    elif alternative == "greater":
+        RVAL.p_value  = pval_greater
+        RVAL.conf_int = [
+            (p_L(Y,X+Y,1-conf_level)*M)/(N*(1-p_L(Y,X+Y,1-conf_level))), Inf]
+    elif alternative == "two.sided":
+        RVAL.p_value  = min( (1, 2*min(pval_less,pval_greater) ) )
+        RVAL.conf_int = [
+            (p_L(Y,X+Y,(1-conf_level)/2)*M)/(N*(1-p_L(Y,X+Y,(1-conf_level)/2))), 
+            (p_U(Y,X+Y,(1-conf_level)/2)*M)/(N*(1-p_U(Y,X+Y,(1-conf_level)/2)))]
+    
+    RVAL.conf_int.append(conf_level)
+    return RVAL
+
+if __name__=="__main__":
+    #n = 17877
+    #m = 16660
+    #print(test((2,9), (n,m)))
+
+    print(docstring)
